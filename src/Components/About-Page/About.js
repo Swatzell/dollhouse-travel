3dfc97e2657ee38c657c4952c4246f923bbd56e3
David Swatzell <thatsstrikethree@gmail.com>
2024-06-01 17:39:38 -0500

    add placeholder text and imports/exports to about page

---
+++ src/Components/About-Page/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './About.css';
+
+const About = () => {
+    return (
+        <div className="about">
+            <h1>About Dollhouse</h1>
+            <p>Dollhouse rules.</p>
+        </div>
+    );
+}
+
+export default About;
