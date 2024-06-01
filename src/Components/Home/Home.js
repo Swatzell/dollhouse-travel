ebc613d4105e270dc2e915ff348ec0f0fbb32bfa
David Swatzell <thatsstrikethree@gmail.com>
2024-06-01 17:46:04 -0500

    add placeholders and import/exports to home

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Dollhouse</h1>
+            <p>Welcome to Dollhouse.</p>
+        </div>
+    );
+}
+
+export default Home;
