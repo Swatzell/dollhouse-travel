acba3027366d94a18bcd5441086ac233494a8b8b
David Swatzell <thatsstrikethree@gmail.com>
2024-06-01 17:25:41 -0500

    add NavBar function with links

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,3 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/placestostay">Places to Stay</Link>
+      <Link to="/requestbooking">Request Booking Consultation</Link>
+      <Link to="/blog">Blog</Link>
+      <Link to="/connect">Connect</Link>
+    </nav>
+  );
+}
+
+
+export default NavBar;
