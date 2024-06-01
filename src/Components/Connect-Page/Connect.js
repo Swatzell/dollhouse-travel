1feb7654f049d039eb16978576aaa764bb1c211a
David Swatzell <thatsstrikethree@gmail.com>
2024-06-01 17:44:16 -0500

    change all instances of contact to connect

---
+++ src/Components/Connect-Page/Connect.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Connect.css';
+
+const Connect = () => {
+    return (
+        <div className="Connect">
+            <h1>Connect Dollhouse</h1>
+            <p>Connect with us.</p>
+        </div>
+    );
+}
+
+export default Connect;
