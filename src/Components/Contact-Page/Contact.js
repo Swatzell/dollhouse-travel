47925ab7ce70c5e24f51d3c50c303ba467366c4a
David Swatzell <thatsstrikethree@gmail.com>
2024-06-01 17:42:05 -0500

    add placeholders and import/exports to contact page

---
+++ src/Components/Contact-Page/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Contact.css';
+
+const Contact = () => {
+    return (
+        <div className="contact">
+            <h1>Contact Dollhouse</h1>
+            <p>Connect with us.</p>
+        </div>
+    );
+}
+
+export default Contact;
