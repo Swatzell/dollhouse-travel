1299d4270fcc94302aed8e73b6b2fb4b02d265a0
David Swatzell <thatsstrikethree@gmail.com>
2024-06-05 11:35:29 -0500

    add booking form function

--- src/Components/Request-Booking/Request-Booking.js
+++ src/Components/Request-Booking/Request-Booking.js
@@ -4,3 +4,25 @@ import emailjs from 'emailjs-com';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import "./Request-Booking.css";
+
+const BookingForm = ({ serviceID, templateID, userID, recipientEmail }) => {
+    const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      address: '',
+      city: '',
+      state: '',
+      zipCode: '',
+      numberOfAdults: '',
+      numberOfChildren: '',
+      preferredStartDate: null,
+      preferredEndDate: null,
+      preferredHotel: '',
+      secondHotelPreference: '',
+      specialRequests: '',
+    });
+  
+    const [errors, setErrors] = useState({});
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    
